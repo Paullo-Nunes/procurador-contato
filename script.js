4e89b7c2989b32ff14fd7772e71c69efbc60e686
Dev Js <alienwaredev369@outlook.com>
2026-03-29 17:32:19 -0300

    primeiro commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Mapeando os elementos
+let p = document.querySelector("p")
+let input = document.querySelector("input")
+
+const contacts = [
+    {name: 'Paulo', number: '(11) 99387-5643' },
+    {name: 'Rafaela', number: '(82) 99863-0989'},
+    {name: 'Flávia', number: '(83) 97865-0032'},
+    {name: 'Caroline', number: '(81) 96437-6545'}
+
+]
+
+function search() {
+    for (let i = 0; i < contacts.length; i++) {
+        if (input.value === contacts[i].name) {
+            p.innerHTML = `Contato encontrado: ${contacts[i].name} Tel.: ${contacts[i].number}`
+            break
+            
+        } else {
+            p.innerHTML= `Contato não encontrado, tente novamente.`
+        } 
+        
+    }
+}
